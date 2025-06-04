978b1444feba844152cf3106b56e2b58512c1601
Alokik Garg <alokikgarg24@gmail.com>
2025-06-04 15:49:04 +0530

    update allowed hosts

--- DjangoProject2/settings.py
+++ DjangoProject2/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+
 from decouple import config
 import dj_database_url
-
+from os.path import join, dirname
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
